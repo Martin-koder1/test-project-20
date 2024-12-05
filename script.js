f56faa01c87cfe712373ad6e4dcd6cb5c3e5efaf
Martin-koder1 <martinmartinkoder@gmail.com>
2024-12-05 10:35:44 +0100

    buttons

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const todoList = document.querySelector("#todoList");
 
 // Registers when button is clicked and activates a function.
 addButton.addEventListener("click", function (){
-    console.log("Knappen er trykket!");
+    console.log("Leg til knappen er trykket!");
 
     // Creating a variable storing the value of another variable
     // Taking the global variable inputField and using the method .value to fetch a value stored in a new variable.
@@ -16,4 +16,22 @@ addButton.addEventListener("click", function (){
     const todoItem = document.createElement("li");
     todoItem.textContent = inputFieldValue;
     todoList.appendChild(todoItem);
+
+    const finishedButton = document.createElement("button");
+    finishedButton.textContent = "Ferdig";
+    todoItem.appendChild(finishedButton);
+
+    finishedButton.addEventListener("click", function () {
+        console.log("Ferdi knappen er trykket!");
+        todoItem.style.textDecoration = "line-through";
+    });
+
+    const deleteButton = document.createElement("button")
+    deleteButton.textContent = "Slett";
+    todoItem.appendChild(deleteButton);
+
+    deleteButton.addEventListener("click", function () {
+        console.log("Slett knappen er trykket!");
+        todoItem.remove();
+    });
 });
